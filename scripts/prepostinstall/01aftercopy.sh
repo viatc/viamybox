e1a648e334024e17bab8dcaf4a7426ae50b9b217
viatc <39264641+viatc@users.noreply.github.com>
2020-01-27 11:47:59 +0300

    Update 01aftercopy.sh

--- scripts/prepostinstall/01aftercopy.sh
+++ scripts/prepostinstall/01aftercopy.sh
@@ -25,7 +25,7 @@ cd $VIADIR/conffiles/mjpg-streamer/mjpg-streamer-experimental
 make
 make install
 cp $VIADIR/scripts/mjpg-streamerd.sh /etc/init.d/
-update-rc.d -f mjpg-streamerd.sh defaults
+update-rc.d -f mjpg-streamerd.sh disable
 
 #gstreamer
 cd $VIADIR/scripts/gstreamer-record
