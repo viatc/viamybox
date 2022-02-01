2cffae60319e387cd3034442a201464831011f74
viatc <viatc.msk@gmail.com>
2022-02-01 15:40:51 +0300

    modified:   www/scripts/via_rec_video_gstrm.sh 	www/scripts/off_radio.sh 	www/scripts/on_radio.sh

--- www/scripts/via_rec_video_gstrm.sh
+++ www/scripts/via_rec_video_gstrm.sh
@@ -11,6 +11,8 @@
 	##  
 #recording video in mkv via gstreamer via-rec-video.c
 
+FILECONF="/home/pi/viamybox/conffiles/via.conf"
+VIADIR="/home/pi/viamybox"
 EXECFILE="/sbin/via-rec-av-c270"
 StoreVideo="/home/pi/camera/video"
 # wait_param=$(echo "scale=4;0.04" | bc)
@@ -26,6 +28,15 @@ sudo sed "/$2/s/$3/$4/g" $FILE > $FILE.new
 sudo mv -f $FILE.new $FILE
 }
 
+function checkSoundUsbCameraIsBusy {
+numCaptureDevice=$(grep "audioCaptureDevice" $FILECONF |awk '{print $2}')
+arecord --device plughw:"$numCaptureDevice",0 -s 1 /dev/null 
+if [ $? -eq 1 ]; then 
+	echo "USB camera sound IS BUSY. Reload usb devices..."
+	$VIADIR/scripts/resetusb
+fi
+}
+
 proc=$(echo $EXECFILE|awk -F/ '{print $3}')
 ret=$(ps aux | grep $proc  | wc -l)
 echo "$ret"
@@ -33,6 +44,7 @@ if [ "$ret" -eq 1 ]
 then {
 	SubstParamInFile '/home/pi/viamybox/www/style.css' 'background-image:' 'rec-av.png' 'rec-red-av.gif'
 	chown www-data:www-data /home/pi/viamybox/www/style.css
+    checkSoundUsbCameraIsBusy
 	$EXECFILE
 }
 else {
