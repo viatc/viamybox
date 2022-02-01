3829e4adfcd2f5b6477d0346bf2fe1fa0b5c0188
viatc <viatc.msk@gmail.com>
2022-02-01 16:57:34 +0300

    fix errors

--- scripts/control-services.sh
+++ scripts/control-services.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-	## Copyright (C) 2017-2019 ViaMyBox viatc.msk@gmail.com
-	## This file is a part of ViaMyBox free software: you can redistribute it and/or modify
-	## it under the terms of the GNU General Public License as published by
-	## the Free Software Foundation, either version 3 of the License, or
-	## any later version.
-	##
-	## You should have received a copy of the GNU General Public License
-	## along with ViaMyBox in /home/pi/COPIYNG file.
-	## If not, see <https://www.gnu.org/licenses/>.
-	##  
+## Copyright (C) 2017-2019 ViaMyBox viatc.msk@gmail.com
+## This file is a part of ViaMyBox free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## any later version.
+##
+## You should have received a copy of the GNU General Public License
+## along with ViaMyBox in /home/pi/COPIYNG file.
+## If not, see <https://www.gnu.org/licenses/>.
+##  
 	
-	## This is part of code based on project:
-	## https://github.com/crcerror/ES-generic-shutdown/blob/master/multi_switch.sh
-	## Many thanks!
+## This is part of code based on project:
+## https://github.com/crcerror/ES-generic-shutdown/blob/master/multi_switch.sh
+## Many thanks!
 	
 # a1='/home/pi/viamybox/scripts/via-mybox-func.sh' ; source "$a1" ; if [ $? -ne 0 ] ; then
 # echo "no function library $a1" 1>&2 ; exit 1 ; fi
